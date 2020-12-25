d2ccc19b99325e1a3ccc71bd6f542597c708a8bc
OrangeX4 <318483724@qq.com>
2020-12-25 21:29:02 +0800

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,17 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['POST'])
 def hello_world():
-    print(request.get_json())
+    json = request.get_json()
+    for key, keyPoint in json['keyPoints'].items():
+        print('{}: {} {}'.format(
+            key, keyPoint['position']['x'], keyPoint['position']['y']))
     return 'Success!'
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
+    # $env:FLASK_APP = "server.py"
+    # flask run --host=0.0.0.0
