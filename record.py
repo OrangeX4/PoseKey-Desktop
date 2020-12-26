396e61a49a0c422ad35277134278d040f4d68c94
OrangeX4 <318483724@qq.com>
2020-12-26 21:27:45 +0800

    Add record.py

--- record.py
+++ record.py
@@ -1,11 +1,9 @@
 from flask import Flask, request
-from tensorflow import keras
+
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def post():
-    probability_model = keras.models.load_model('model.h5')
-    # predictions = probability_model.predict(test_images)
     json = request.get_json()
     print(json['time'])
     with open('data.json', 'a', encoding='utf-8') as f:
